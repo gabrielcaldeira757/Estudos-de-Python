95c3002f666d0d87e7b00b12bfd3895456f319e8
gabrielcaldeira757 <gabrielcaldeira757@gmail.com>
2018-07-16 15:22:48 -0300

    Alteração para acesso
    acesso The piratbay correto; next step-> conseguir separar os links do html

--- baixaSerie.py
+++ baixaSerie.py
@@ -5,14 +5,24 @@ import urllib.parse
 try:
     pesquisa = str(input('digite a pesquisa no piratbay'))
     url = 'http://thepiratebay.org/'
-    dic = {'q': pesquisa, 'submit': 's'}   
-    dta = urllib.parse.urlencode(dic)    
-    dta = dta.encode('utf-8')    
-    results = urllib.request.Request(url, dta)    
-    resposta = urllib.request.urlopen(results)    
-    texto = resposta.read()    
-    magnetico = texto.find('"magnet:')    
-    magnetico = magnetico + 15
-    print(texto)
+    headers = {}
+    headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+    dic = {'q': pesquisa, 'submit': 's'}
+    dta = urllib.parse.urlencode(dic)
+    dta = dta.encode('utf-8')
+    results = urllib.request.Request(url, dta, headers=headers)
+    resposta = urllib.request.urlopen(results)
+    texto = str(resposta.read())
+    magnetico = texto.find('"magnet:')
+    saveFile = open('codethe.txt', 'w')
+    print('linha1')
+    saveFile.write(texto)
+    print('linha1')
+    saveFile.close()
+    print('linha1')
+    
 except Exception as e:
-    print(str(e))
+    print(str(e)) 
+    
+
+
