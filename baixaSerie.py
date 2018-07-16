bd59ecd7f2c4ea80a3d1d10b65dfdfdc1fa17c7a
gabrielcaldeira757 <gabrielcaldeira757@gmail.com>
2018-07-16 16:21:21 -0300

    Add pesquisa  via url

--- baixaSerie.py
+++ baixaSerie.py
@@ -3,14 +3,26 @@ import urllib.parse
 
 
 try:
-    pesquisa = str(input('digite a pesquisa no piratbay'))
-    url = 'http://thepiratebay.org/'
+    imp = str(input('digite a pesquisa no piratbay'))
+    url = 'https://thepiratebay.org/search'
+    k= 0
+    pesquisa = "";
+    while  k<len(imp):
+        if imp[k]in ' ':
+            pesquisa = pesquisa + "%20"
+        else:
+            pesquisa = pesquisa + imp[k]
+        k+=1
+    url = url + '/' + pesquisa
+    print(url)
+
     headers = {}
     headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
-    dic = {'q': pesquisa, 'submit': 's'}
-    dta = urllib.parse.urlencode(dic)
+
+    
+    dta = urllib.parse.urlencode(url)
     dta = dta.encode('utf-8')
-    results = urllib.request.Request(url, dta, headers=headers)
+    results = urllib.request.Request(url,, headers=headers)
     resposta = urllib.request.urlopen(results)
     texto = str(resposta.read())
     magnetico = texto.find('"magnet:')
