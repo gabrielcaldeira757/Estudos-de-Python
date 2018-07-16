da215deb64ce12ed2fb29a79bbe69a331102cdb6
gabrielcaldeira757 <gabrielcaldeira757@gmail.com>
2018-07-16 11:04:09 -0300

    acesso ao piratebay
    erro 403

--- baixaSerie.py
+++ baixaSerie.py
@@ -1,11 +1,18 @@
 import urllib.request
-
-url = "https://thepiratebay.org/"
-def conectaPagina():
-	pagina = urllib.request.urlopen(url)
-	texto = pagina.read().decode('utf8')
-	print(texto)
+import urllib.parse
 
 
-conectaPagina()
-
+try:
+    pesquisa = str(input('digite a pesquisa no piratbay'))
+    url = 'http://thepiratebay.org/'
+    dic = {'q': pesquisa, 'submit': 's'}   
+    dta = urllib.parse.urlencode(dic)    
+    dta = dta.encode('utf-8')    
+    results = urllib.request.Request(url, dta)    
+    resposta = urllib.request.urlopen(results)    
+    texto = resposta.read()    
+    magnetico = texto.find('"magnet:')    
+    magnetico = magnetico + 15
+    print(texto)
+except Exception as e:
+    print(str(e))
